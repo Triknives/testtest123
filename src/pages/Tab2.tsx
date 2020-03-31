f563155b6ae89a5e34e49cf4225dacc5ed599320
lioness3 <jcnailtech3@yahoo.com>
2020-03-31 12:33:14 -0700

    init tabs

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+const Tab2: React.FC = () => {
+  return (
+    <IonPage>
+
+
+    </IonPage>
+  );
+};
+
+export default Tab2;
